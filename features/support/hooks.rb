25007da8f53048b9b710c7e0ded17162cf54307b
Abraham Siregar <siregara@bhf.org.uk>
2019-11-04 13:22:39 +0000

    adding hook that maximises window before each test

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,6 +3,7 @@
 # TestBrowser.browser = Watir::Browser.new (ENV['BROWSER'] || 'chrome').to_sym
 TestBrowser.browser = Watir::Browser.new (ENV['BROWSER'] || 'chrome').to_sym, options: {options: {'useAutomationExtension' => false}}
 Before do
+  TestBrowser.browser.window.maximize
   TestBrowser.browser.cookies.clear
   @@new_acc_email = "#{SecureRandom.hex 8}@mailsac.com"
   # @@new_acc_email = "9256667b1139c660@test.co.uk"
