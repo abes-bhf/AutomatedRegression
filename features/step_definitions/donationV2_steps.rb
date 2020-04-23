5410b9f0988c9a6b5d3e0be6fbefb54f1d23614a
Husna Ahad <ahadh@bhf.org.uk>
2020-04-23 15:39:14 +0100

    hos and donate v2

---
+++ features/step_definitions/donationV2_steps.rb
@@ -0,0 +1,19 @@
+Given("I navigate to the donate page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I select continue without selecting donation type") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("donate widget should return with validation messages") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I enter an invalid amount in other amount") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be shown error messages") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/step_definitions/hos_steps.rb
@@ -0,0 +1,27 @@
+Given("I navigate to heart of steel page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I select continue without selecting {int} and over") do |int|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("a validation message should appear") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I select continue without filling in engraving details") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("engraving details validation message should appear") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I select I am over {int}") do |int|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I enter invalid data on engraving details") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
