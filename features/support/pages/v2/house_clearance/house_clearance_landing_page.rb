8f0936250cec3fc520e4488890320821ef04443f
Austingr <austingr@bhf.org.uk>
2020-09-22 16:26:46 +0100

    House clearance

---
+++ features/support/pages/v2/house_clearance/house_clearance_landing_page.rb
@@ -0,0 +1,44 @@
+class HouseClearanceLandingPage < GenericPage
+
+  attr_accessor :trait, :url
+
+  def initialize(browser)
+    super
+    @url = EnvConfig.base_url + "kris-hc-sample"
+  end
+
+
+
+  def trait
+    trait = browser.h1(itemprop: 'headline', text: "Beat heartbreak forever")
+    return trait
+  end
+
+
+  def postcode_field_v2
+    postcode_field_v2 = browser.input(name: /^*Postcode$/)
+    return postcode_field_v2
+  end
+
+  def submit_postcode
+    details = EnvConfig.data['formsV2_data']['formsV2_details']
+    Watir::Wait.until {postcode_field_v2.present?}
+        begin
+          retries ||= 0
+          postcode_field_v2.send_keys (details['postcode'])
+        rescue Selenium::WebDriver::Error::ElementClickInterceptedError
+          retry if (retries += 1) < 3
+        end
+    Watir::Wait.until {browser.div(class: "is-cta").button.present?}
+        begin
+          retries ||= 0
+          browser.div(class: "is-cta").button.click
+        rescue Selenium::WebDriver::Error::ElementClickInterceptedError
+          retry if (retries += 1) < 3
+        end
+  end
+
+
+
+
+end
